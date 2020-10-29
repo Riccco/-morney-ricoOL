c2ab40779ca4172d16629f7023d1114cddd93f2d
Rico <5924435@qq.com>
2020-10-29 16:10:55 +0800

    deploy

---
+++ precache-manifest.48513316ae1ed99bb6b38b8cdc0d25ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c955278ea42dad528be",
+    "url": "/-morney-ricoOL/css/app.98bdc2a2.css"
+  },
+  {
+    "revision": "36599c2108687e9cae1f141d27d1ca8b",
+    "url": "/-morney-ricoOL/img/notFound.36599c21.png"
+  },
+  {
+    "revision": "c9e51e7d4b35643280ba501fc2ab10f0",
+    "url": "/-morney-ricoOL/index.html"
+  },
+  {
+    "revision": "6c955278ea42dad528be",
+    "url": "/-morney-ricoOL/js/app.80620b24.js"
+  },
+  {
+    "revision": "e99da596d9a1189408a6",
+    "url": "/-morney-ricoOL/js/chunk-vendors.e372f80d.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/-morney-ricoOL/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/-morney-ricoOL/robots.txt"
+  }
+]);
